44d2acfca073f8000b11448e22e66403291dd573
williamyap90 <william.yap90@gmail.com>
2021-10-01 10:54:55 +0100

    add eventListener on links to close nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,20 @@
 // animated hamburger menu
-function myFunction(x) {
-  x.classList.toggle("change");
+function toggleClick(x) {
+    x.classList.toggle('open');
 }
+
+const hamburger = document.querySelector('.hamburger');
+const navToggle = document.querySelector('.nav-toggle');
+
+function toggleMenu() {
+    if (hamburger.classList.contains('open')) {
+        hamburger.classList.toggle('open');
+        navToggle.checked = false;
+    }
+}
+
+const hamburgerLinks = document.querySelectorAll('.nav__item');
+
+hamburgerLinks.forEach((link) => {
+    link.addEventListener('click', toggleMenu);
+});
